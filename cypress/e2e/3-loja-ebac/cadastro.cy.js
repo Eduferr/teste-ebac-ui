0dd4ba195c7c70a329eb68fb61a5d21989113c2f
Eduferr <eduferr2803@gmail.com>
2024-10-31 20:11:12 -0300

    Faker e Variáveis

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: cadastro', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('Deve completar o cadastro com sucesso', () => {
+        //Usando email fake
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('teste@123')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        //Complentando o cadastro
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        //Tempo de espera na tela, para verificar alguma informação
+        cy.wait(5000)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
+    });
+
+    it.only('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+
+        var nome = faker.person.firstName() //gerando o nome primeiro, para ser mesmo no email
+        var email = faker.internet.email(nome) 
+        var sobreNome = faker.person.lastName()
+        var senha = 'teste@123'
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type(senha)
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobreNome)
+        cy.wait(5000)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
+    });
+});
