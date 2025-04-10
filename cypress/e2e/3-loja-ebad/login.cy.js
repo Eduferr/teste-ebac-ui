a0866a2e84642ffc9ff59166acae722b7a0d840b
Eduferr <eduferr2803@gmail.com>
2025-04-10 10:02:00 -0300

    Testes negativos e hooks

--- cypress/e2e/3-loja-ebad/login.cy.js
+++ cypress/e2e/3-loja-ebad/login.cy.js
@@ -2,12 +2,38 @@
 
 //suite
 describe('funcionalidade login', () => {
-    //cenário de teste
-    it('Fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+   
+   //hooks
+   beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+   });
+   afterEach(() => {
+    cy.screenshot();
+    });
+   
+   
+    //cenários de teste
+
+    it('Fazer login com sucesso', () => {        
         cy.get('#username').type('edu.teste@teste.com.br')
         cy.get('#password').type(123456)
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'edu.teste-0551')
     });
+    //validar email
+    it('Erro de acesso, ao inserir um usuário inválido', () => {
+        cy.get('#username').type('eduteste@teste.com.br')
+        cy.get('#password').type(123456)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
+    });
+    //Validar senha
+    it('Erro de acesso, ao inserir senha inválida', () => {
+        cy.get('#username').type('edu.teste@teste.com.br')
+        cy.get('#password').type(12345)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida ')
+    });
+
+
 });
