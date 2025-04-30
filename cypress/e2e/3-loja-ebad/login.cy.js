1f57aaa1a015fe0c0c95e5b00b2d88aa15f4eb3e
Eduferr <eduferr2803@gmail.com>
2025-04-30 10:20:52 -0300

    usando baseUrl e fixtures

--- cypress/e2e/3-loja-ebad/login.cy.js
+++ cypress/e2e/3-loja-ebad/login.cy.js
@@ -1,39 +1,55 @@
 /// <reference types="cypress"/>
 
+const perfil = require('../../fixtures/perfil.json')
+
 //suite
 describe('funcionalidade login', () => {
    
    //hooks
    beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('/minha-conta') //baseUrl - Está em cypresse.config.js
    });
    afterEach(() => {
     cy.screenshot();
     });
-   
-   
+      
     //cenários de teste
-
-    it('Fazer login com sucesso', () => {        
+    it.skip('Fazer login com sucesso', () => {        
         cy.get('#username').type('edu.teste@teste.com.br')
         cy.get('#password').type(123456)
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'edu.teste-0551')
     });
     //validar email
-    it('Erro de acesso, ao inserir um usuário inválido', () => {
+    it.skip('Erro de acesso, ao inserir um usuário inválido', () => {
         cy.get('#username').type('eduteste@teste.com.br')
         cy.get('#password').type(123456)
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
     });
     //Validar senha
-    it('Erro de acesso, ao inserir senha inválida', () => {
+    it.skip('Erro de acesso, ao inserir senha inválida', () => {
         cy.get('#username').type('edu.teste@teste.com.br')
         cy.get('#password').type(12345)
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida ')
     });
-
+    
+    // Importando massa de dados
+    it.skip('Efetuar login com sucesso - Usando massa de dados', () => {        
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha,{log: false})// log: para ocultar dados
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'edu.teste-0551')
+    });
+    //Usando a fixtures de forma nativa
+    it('Efetuar login com sucesso - Usando fixtures', () => {        
+        cy.fixture('perfil').then(dados=> {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha,{log: false})// log: para ocultar dados
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'edu.teste-0551')
+        })
+    });
 
 });
