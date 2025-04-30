27d6d7a8466eedd1bcc4060c2710f1ce504e5b57
Eduferr <eduferr2803@gmail.com>
2025-04-30 09:19:44 -0300

    produtos

---
+++ cypress/e2e/3-loja-ebad/produtos.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first() // captura o primeiro item da lista  
+            //.last()   // captura o último item da lista
+            //.eq(2)  // pegando um item pela posição
+            .contains('Apollo Running Short') // buscando pelo nome dentro do bloco
+            .click()
+        cy.get('#tab-title-description > a').should('contain','Descrição')
+    });
+});
